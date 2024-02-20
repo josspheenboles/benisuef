92f8e3ba51e35bc45612f5228ade83a8992c23f1
jboles <eng.jospheenboles@gmail.com>
2024-02-20 23:46:47 +0200

    database configuration

--- benisuef/settings.py
+++ benisuef/settings.py
@@ -78,8 +78,14 @@ WSGI_APPLICATION = 'benisuef.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME':'beni',
+        'USER':'iti',
+        'PASSWORD':'123',
+        'HOST':'localhost',
+        'PORT':5432
     }
 }
 
