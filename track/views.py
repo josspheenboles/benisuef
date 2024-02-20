4e6794a98b1e65c8377075871afde88e6559a0f5
jboles <eng.jospheenboles@gmail.com>
2024-02-21 00:02:43 +0200

    model get all data

--- track/views.py
+++ track/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from  django.http import HttpResponse
+from .models import *
 # Create your views here.
 def hello(request):
     return HttpResponse('hello from track app')
 def alltracks(request):
     # return HttpResponse('alltracks')
-    return render(request,'track/list.html')
+    tracks=Track.objects.all()
+    context={'tracks':tracks}
+
+    return render(request,'track/list.html',context)
 def trackbyid(request,id):
     # return HttpResponse(f'trackbyid{id}')
     return  render(request,'track/showtrack.html')
